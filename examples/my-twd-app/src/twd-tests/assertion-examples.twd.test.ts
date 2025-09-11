fac10784424a620c0508efb27f225b235e7010ab
kevinccbsg <kevinccbsg@gmail.com>
2025-09-11 23:02:56 +0200

    feat: add basic url command

--- examples/my-twd-app/src/twd-tests/assertion-examples.twd.test.ts
+++ examples/my-twd-app/src/twd-tests/assertion-examples.twd.test.ts
@@ -52,4 +52,8 @@ it("assertion examples", async () => {
   // be.selected
   const beSelected = await twd.get("#be-selected option[value='two']");
   beSelected.should("be.selected");
+
+  // contain.url
+  await twd.url().should("eq", "http://localhost:3000/assertions");
+  await twd.url().should("contain.url", "/assertions");
 });
--- src/asserts/index.ts
+++ src/asserts/index.ts
@@ -1,3 +1,4 @@
+import { assertionMessage } from "../utils/assertionMessage";
 import { AnyAssertion, AssertionName } from "../twd-types";
 
 function isVisible(el: HTMLElement): boolean {
@@ -12,20 +13,6 @@ function isVisible(el: HTMLElement): boolean {
   }
   return true;
 }
-
-const assertionMessage = (valid: boolean, isNegated: boolean, correctMessage: string, errorMessage: string) => {
-  if (!valid && !isNegated) {
-    throw new Error(errorMessage);
-  } else if (valid && isNegated) {
-    throw new Error(
-      errorMessage
-        .replace("to be", "to not be")
-        .replace("to have", "to not have")
-        .replace("to contain", "to not contain")
-    );
-  }
-  return correctMessage;
-};
 
 export const runAssertion = (
   el: Element,
