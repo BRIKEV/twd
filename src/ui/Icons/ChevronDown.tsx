90240338009d309fd73998de3e69e32452579af3
kevinccbsg <kevinccbsg@gmail.com>
2025-09-21 20:49:11 +0200

    refactor: remove code duplication

--- src/ui/Icons/ChevronDown.tsx
+++ src/ui/Icons/ChevronDown.tsx
@@ -1,19 +1,9 @@
+import BaseIcon from "./BaseIcon";
+
 const ChevronDown = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className="lucide lucide-chevron-down-icon lucide-chevron-down"
-    data-testid="chevron-down-icon"
-  >
+  <BaseIcon className="lucide-chevron-down-icon lucide-chevron-down" dataTestId="chevron-down-icon">
     <path d="m6 9 6 6 6-6" />
-  </svg>
+  </BaseIcon>
 );
 
 export default ChevronDown;
--- src/ui/Icons/ChevronRight.tsx
+++ src/ui/Icons/ChevronRight.tsx
@@ -1,19 +1,9 @@
+import BaseIcon from "./BaseIcon";
+
 const ChevronRight = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className="lucide lucide-chevron-right-icon lucide-chevron-right"
-    data-testid="chevron-right-icon"
-  >
+  <BaseIcon className="lucide-chevron-right-icon lucide-chevron-right" dataTestId="chevron-right-icon">
     <path d="m9 18 6-6-6-6" />
-  </svg>
+  </BaseIcon>
 );
 
 export default ChevronRight;
