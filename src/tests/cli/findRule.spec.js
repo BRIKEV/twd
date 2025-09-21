bd56ed89a086c3d28a3e9365f06180d92736a0b4
kevinccbsg <kevinccbsg@gmail.com>
2025-09-21 23:47:01 +0200

    fix: intercept path error

--- src/tests/cli/findRule.spec.js
+++ src/tests/cli/findRule.spec.js
@@ -14,10 +14,51 @@ describe('findRule', () => {
 
   it('matches by regex url', () => {
     const rules = [
-      { method: 'GET', url: /^https:\/\/foo\..+/, alias: 'a' },
+      { method: 'GET', url: 'https://foo.com', alias: 'a' },
+    ];
+    expect(findRule('GET', /^https:\/\/foo\..+/, rules)).toEqual(rules[0]);
+    expect(findRule('GET', 'https://bar.com', rules)).toBeUndefined();
+  });
+
+  it('returns the first matching rule', () => {
+    const rules = [
+      { method: 'GET', url: 'https://foo.com', alias: 'first' },
+      { method: 'GET', url: 'https://foo.com', alias: 'second' },
     ];
     expect(findRule('GET', 'https://foo.com', rules)).toEqual(rules[0]);
-    expect(findRule('GET', 'https://foo.org', rules)).toEqual(rules[0]);
-    expect(findRule('GET', 'https://bar.com', rules)).toBeUndefined();
+  });
+
+  it('should match by contains url', () => {
+    const rules = [
+      { method: 'GET', url: 'https://foo.com/api', alias: 'a' },
+    ];
+    expect(findRule('GET', 'https://foo.com/api', rules)).toEqual(rules[0]);
+    expect(findRule('GET', 'https://foo.com/api/users', rules)).toBeUndefined();
+    expect(findRule('GET', 'https://foo.com/home', rules)).toBeUndefined();
+  });
+
+  it('should match by regex', () => {
+    const rules = [
+      { method: 'GET', url: 'https://foo.com/api/users/preferences', alias: 'a' },
+    ];
+    expect(findRule('GET', /\/users\/*/, rules)).toEqual(rules[0]);
+  });
+
+  it('should match by contains some path of the url', () => {
+    const rules = [
+      { method: 'GET', url: 'https://foo.com/api', alias: 'a' },
+    ];
+    expect(findRule('GET', '/api', rules)).toEqual(rules[0]);
+    expect(findRule('GET', '/api/users', rules)).toBeUndefined();
+    expect(findRule('GET', 'https://foo.com/home', rules)).toBeUndefined();
+  });
+
+  it('is case-insensitive for method', () => {
+    const rules = [
+      { method: 'get', url: 'https://foo.com', alias: 'a' },
+    ];
+    expect(findRule('GET', 'https://foo.com', rules)).toEqual(rules[0]);
+    expect(findRule('get', 'https://foo.com', rules)).toEqual(rules[0]);
+    expect(findRule('Get', 'https://foo.com', rules)).toEqual(rules[0]);
   });
 });
