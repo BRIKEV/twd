c3404d737fcd933c2a424ebe0127950951bba6a2
Kevin Julián Martínez Escobar <kevinccbsg@gmail.com>
2025-10-06 21:04:43 +0200

    fix: remove truncate chai expects (#39)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export * from './twd';
 export { TWDSidebar } from './ui/TWDSidebar';
+import { config } from 'chai';
 export { expect } from 'chai';
 export { userEvent } from './proxies/userEvent';
+
+config.truncateThreshold = 0;
