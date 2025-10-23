13b1a2711072d601c51d0730d743ef9ceb15b3cc
kevinccbsg <kevinccbsg@gmail.com>
2025-10-23 22:58:41 +0200

    feat: add support to other types

--- examples/my-twd-app/vite.config.ts
+++ examples/my-twd-app/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import { removeMockServiceWorker } from '../../src';
 
 // https://vite.dev/config/
 export default defineConfig({
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ export { TWDSidebar } from './ui/TWDSidebar';
 export { initTests } from './initializers/initTests';
 export { expect } from 'chai';
 export { userEvent } from './proxies/userEvent';
-export { removeMockServiceWorker } from './plugin/removeMockServiceWorker';
+// export { removeMockServiceWorker } from './plugin/removeMockServiceWorker';
 
 config.truncateThreshold = 0;
