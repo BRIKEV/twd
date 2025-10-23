2233304597026b12a6bb1825cb7239618266ac73
kevinccbsg <kevinccbsg@gmail.com>
2025-10-23 23:01:37 +0200

    fix: error exporting

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-export * from './twd';
+export {
+  beforeEach,
+  describe,
+  it,
+  itOnly,
+  itSkip,
+  twd,
+} from './twd';
 import { config } from 'chai';
 export { TWDSidebar } from './ui/TWDSidebar';
 export { initTests } from './initializers/initTests';
 export { expect } from 'chai';
 export { userEvent } from './proxies/userEvent';
-// export { removeMockServiceWorker } from './plugin/removeMockServiceWorker';
+export { removeMockServiceWorker } from './plugin/removeMockServiceWorker';
 
 config.truncateThreshold = 0;
