c1958adfbad817b29d2eb537a5abed7ec3ab5c36
kevinccbsg <kevinccbsg@gmail.com>
2025-11-01 07:55:08 +0100

    docs: add more examples

--- examples/twd-test-app/src/main.tsx
+++ examples/twd-test-app/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import router from './routes'
+import router from './routes.tsx'
 import { RouterProvider } from 'react-router'
 
 // Only load the test sidebar and tests in development mode
