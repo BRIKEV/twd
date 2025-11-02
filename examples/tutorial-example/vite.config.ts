bb697b8c681e90ada7de2c24ccc40345f9cc417e
kevinccbsg <kevinccbsg@gmail.com>
2025-11-02 21:08:13 +0100

    docs: add coverage example

--- examples/tutorial-example/vite.config.ts
+++ examples/tutorial-example/vite.config.ts
@@ -4,10 +4,20 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { configDefaults } from "vitest/config"
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    istanbul({
+      include: 'src/**/*',
+      exclude: ['node_modules', 'tests/'],
+      extension: ['.ts', '.tsx'],
+      requireEnv: process.env.CI ? true : false,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
