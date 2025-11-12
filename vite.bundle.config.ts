c8e0a76200cb76a60a66c390687d57f34b6b7652
kevinccbsg <kevinccbsg@gmail.com>
2025-11-13 00:18:39 +0100

    fix: error bundle file was removing all dist

--- vite.bundle.config.ts
+++ vite.bundle.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
         },
       },
     },
-
+    emptyOutDir: false,
     // optional, ensures smaller modern output
     target: "esnext",
     minify: "esbuild",
