2dd3fff296d56dfe83af6093609e8e3e1ae88ad0
kevinccbsg <kevinccbsg@gmail.com>
2025-11-14 16:59:49 +0100

    feat: add ci execution example

--- examples/twd-test-app/vite.config.ts
+++ examples/twd-test-app/vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { removeMockServiceWorker } from '../../src/vite-plugin';
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), removeMockServiceWorker()],
+  plugins: [
+    react(),
+    removeMockServiceWorker(),
+    // configure istanbul plugin
+    istanbul({
+      include: 'src/**/*',
+      exclude: ['node_modules', 'dist', 'twd-tests/**'],
+      extension: ['.ts', '.tsx'],
+      requireEnv: process.env.CI ? true : false,
+    }),
+  ],
 })
