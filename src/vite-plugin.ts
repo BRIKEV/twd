4175583df6e647bf4cb2a64a9df2554fad5dc7f0
kevinccbsg <kevinccbsg@gmail.com>
2026-09-06 11:47:58 +0200

    feat(layout): add the twdSnapshot vite plugin

--- src/vite-plugin.ts
+++ src/vite-plugin.ts
@@ -1,3 +1,4 @@
 export { removeMockServiceWorker } from './plugin/removeMockServiceWorker';
 export { twdHmr, type TwdHmrOptions } from './plugin/twdHmr';
 export { twd, type TwdPluginOptions } from './plugin/twd';
+export { twdSnapshot, type TwdSnapshotOptions } from './plugin/twdSnapshot';
